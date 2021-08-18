3a281ee1dbb9f9bf07135437d382db998a4f2cd5
ryonryon1802 <ryo.hskw0812@gmail.com>
2021-08-18 16:31:40 +0900

    [add] 追加ページから店名を検索する機能を追加した（コントローラとモデルの連携はしていない）

--- app/assets/javascripts/find_locations.js
+++ app/assets/javascripts/find_locations.js
@@ -1,22 +1,30 @@
-// function findLocations() {
-//   // console.log("test1")
-//   // document.getElementById('location').value = "test"
-//   var params = {
-//     query: "海神",
-//     key: "AIzaSyAcf1cJuw4-N0o12TMyL0VMypww9f1RUOk"
-//   };
+function findLocations() {
+  var search = document.getElementById("search").value;
+  var params = {
+    query: search,
+    key: "AIzaSyAcf1cJuw4-N0o12TMyL0VMypww9f1RUOk"
+  };
+  var locationList = document.getElementById('location');
+  // locationList.disabled = false;
+  locationList.innerHTML = '';
+  var option = document.createElement('option');
+  option.innerHTML = '店名を選択してください';
+  option.defaultSelected = true;
+  // option.disabled = true;
+  locationList.appendChild(option);
 
-//   // var query_params = new URLSearchParams(params);
-//   // response = fetch(
-//   //   `https://maps.googleapis.com/maps/api/place/textsearch/json?${query_params}`,
-//   //   {mode: 'cors', credentials: 'include'}
-//   //   //{headers: {"x-api-key": "AIzaSyAcf1cJuw4-N0o12TMyL0VMypww9f1RUOk"}}
-//   // );
-//   response = fetch(
-//     "https://maps.googleapis.com",
-//     {mode: 'cors'}
-//   )
-//   console.log(response);
-// }
-
-import axios from 'axios';
+  var query_params = new URLSearchParams(params);
+  fetch(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?${query_params}`,
+    {mode: 'cors'}
+  )
+  .then(response=> response.json())
+  .then(data=> data.results)
+  .then(results=> {
+    results.forEach(location=> {
+      let option = document.createElement('option');
+      option.innerHTML = location.name + location.formatted_address.replace(/日本、〒[0-9]{3}-[0-9]{4}/, "");
+      locationList.appendChild(option);
+    });
+  })
+};
